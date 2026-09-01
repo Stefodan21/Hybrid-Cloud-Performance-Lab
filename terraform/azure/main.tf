f5ca0f6e62ef5edd2f36210386cceb2e0c85e897
Stefodan21 <stefaandavis21@outlook.com>
2026-09-01 13:24:33 -0400

    addded resource blocks for load balancer and public ip address

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -6,10 +6,37 @@ data "azurerm_resource_group" "rg" {
 resource "azure_virtual_network" "vnet" {
     name = "vnettradingeastus001"
     region = var.region
-
+    address_space = ["10.0.0.0/16"]
 }
 
-resource "azure_subnet" "" {}
+resource "azure_subnet" "app" {
+    name = "snatradingeastus001"
+    region = var.region
+    address_space = ["10.0.0.0/24"]
+}
+
+resource "azure_subnet" "db" {
+    name = "sndtradingeastus001"
+    region = var.region
+    address_space = ["10.0.1.0/24"]
+}
 
 
-resource "azure_virtual_machine"
+resource "azurerm_public_ip" "appip" {
+    name = "publiciptradeeastus001"
+    location = var.region
+    resource_group_name = var.resource_group_name
+    allocation_method = "Dynamic"
+    sku = "Standard"
+}
+
+
+resource "azurerm_lb" "lb" {
+    name = "lbtradingeastus001"
+    location = var.region
+    resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "lvmss" {
+    name = "lvmsstradingeastus001"
+}
