a16450f12ba2faa813f142ff2555b2fcbcf7c43f
Stefodan21 <stefaandavis21@outlook.com>
2026-09-02 21:39:50 -0400

    added the frontend ip configuration to the load balancer

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -35,6 +35,11 @@ resource "azurerm_lb" "lb" {
     name = "lbtradingeastus001"
     location = var.region
     resource_group_name = var.resource_group_name
+
+    frontend_ip_configuration {
+        name = "feiptradingeastus001"
+        public_ip_address_id = azurerm_public_ip.appip.id
+    }
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "lvmss" {
